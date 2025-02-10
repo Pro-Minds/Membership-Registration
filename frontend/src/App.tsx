3c8fc2089e9bde02e0fa4b6af012a023c71d0323
micheal-ndoh <michaelndoh9@gmail.com>
2025-02-10 10:30:04 +0100

    deleted dummy.png and updatesd configuration files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Header from './components/templates/Header';
-import Body from './components/templates/Body';
 import Footer from './components/templates/Footer';
-import './App.css';    
-
-// import logo from './logo.sv';
+import RegistrationForm from './components/member/RegistrationForm'; // Import RegistrationForm component
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header /> {/* Render Header component */}
-      <Body />   {/* Render Body component */}
+      <div className="logo-container">
+        <img src="/logo.png" alt="" className="logo" />
+      </div>
+      <RegistrationForm /> {/* Render RegistrationForm component */}
       <Footer /> {/* Render Footer component */}
     </div>
   );
